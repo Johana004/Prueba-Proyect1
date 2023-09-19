7a72bd7d8349668868349abf41b391710ab3ed91
Johana Solano <solanojohana020@outlook.com>
2023-09-18 20:58:06 -0600

    Agregamos carpetas

---
+++ Prueba-1/src/Controller/Prueba1.java
@@ -0,0 +1,14 @@
+
+package Controller;
+
+
+public class Prueba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
